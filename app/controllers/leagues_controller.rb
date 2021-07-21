d911c27c82fac4937b5fe3cc3b1c77531b58c75e
Henry Onu <onuhenry24@gmail.com>
2021-07-21 15:42:43 +0100

    update league controllers with CRUD actions

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,10 +1,28 @@
 class LeaguesController < ApplicationController
+   
     def index
         @leagues = League.all
     end
 
     def new
         @league = League.new
+    end
+
+    def show
+        @league = League.find(params[:id])
+    end
+
+    def edit
+        @league = League.find(params[:id])
+    end
+
+    def update
+        @league = League.find(params[:id])
+        if @league.update(league_params)
+            redirect_to league_path
+        else
+            render :edit
+        end
     end
 
     def create
@@ -16,8 +34,14 @@ class LeaguesController < ApplicationController
         end
     end
 
+    def destroy
+        @league = League.find(params[:id])
+         @league.destroy
+            redirect_to root_path 
+    end
+
     private
     def league_params
-        params.require(:league).permit(:Title, :Text)
+        params.require(:league).permit(:title, :text)
     end
 end
