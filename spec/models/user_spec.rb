13f49118f5874175bfc8829f5e5f25e7cf3d03d0
Henry Onu <onuhenry24@gmail.com>
2021-07-22 10:39:31 +0100

    add test for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:leagues) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(15) }
+  end
+end
