765490c6393c3b34d75142a4416f97bd058a3a95
Henry Onu <onuhenry24@gmail.com>
2021-07-22 10:37:59 +0100

    add validations to user and league models

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,2 +1,5 @@
 class League < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    validates :title, presence: true, length: { mininum: 3, maximum: 20}
+    validates :text, presence: true, length: {maximum: 500}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+      has_many :leagues, foreign_key: :author_id
+      validates :name, presence: true, length: {minimum: 3, maximum: 15}
 end
