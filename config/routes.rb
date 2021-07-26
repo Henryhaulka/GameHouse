5b49715fe03f8a559d8844204b3502c22d19c6fb
Henry Onu <onuhenry24@gmail.com>
2021-07-26 12:07:08 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'categories/index'
   devise_for :users, :controllers => {registrations: 'registrations'}
   root 'leagues#index'
   resources :leagues 
+
+  get "/soccer", to: "leagues#soccer"
+  get "/car_racing", to: "leagues#car_racing"
+  get "/action", to: "leagues#action"
+  get "/adventure", to: "leagues#adventure"
+  get "/logic", to: "leagues#logic"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
