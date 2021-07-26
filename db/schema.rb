dded92d0356e3d047c3d671dd348c36b0cbafb67
Henry Onu <onuhenry24@gmail.com>
2021-07-26 10:17:29 +0100

    add categories table and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_215238) do
+ActiveRecord::Schema.define(version: 2021_07_25_144408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "leagues", force: :cascade do |t|
     t.string "title"
@@ -25,6 +32,16 @@ ActiveRecord::Schema.define(version: 2021_07_23_215238) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "category_id"
+  end
+
+  create_table "relates", force: :cascade do |t|
+    t.bigint "league_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_relates_on_category_id"
+    t.index ["league_id"], name: "index_relates_on_league_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +57,6 @@ ActiveRecord::Schema.define(version: 2021_07_23_215238) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "relates", "categories"
+  add_foreign_key "relates", "leagues"
 end
