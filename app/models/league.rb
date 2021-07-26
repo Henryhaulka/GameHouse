9251ed0073ece9de4e122a6c77aee97b0866f0f3
Henry Onu <onuhenry24@gmail.com>
2021-07-26 15:25:09 +0100

    add relate_sec.rb test

--- app/models/league.rb
+++ app/models/league.rb
@@ -4,6 +4,6 @@ class League < ApplicationRecord
     belongs_to :author, class_name: 'User'
     validates :title, presence: true, length: { mininum: 3, maximum: 20}
     validates :text, presence: true, length: {maximum: 500}
-    has_attached_file :image, styles: { large: "1200x1000", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :image, styles: { large: "1200x1000", medium: "300x300>"}, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
