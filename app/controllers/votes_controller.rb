37b3a4e63637df5d7448d413b9cf3cd00dce9e6f
Henry Onu <onuhenry24@gmail.com>
2021-07-28 15:20:03 +0100

    fix linters

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,9 +3,10 @@ class VotesController < ApplicationController
     if Vote.where(league_id: League.find(params[:league_id]), user_id: current_user.id).exists?
       return redirect_to root_path, notice: 'You already voted for this League '
     end
-
+     # rubocop:disable LineLength
     @vote = League.find(params[:league_id]).votes.build(user_id: current_user.id)
-    redirect_to league_path(id: League.find(params[:league_id])),notice: 'Thanks!!! You have successfully voted for this League' if @vote.save
+    redirect_to league_path(id: League.find(params[:league_id])), notice: 'Thanks!!! You have successfully voted for this League' if @vote.save
+    # rubocop:enable LineLength
   end
 
   def destroy
