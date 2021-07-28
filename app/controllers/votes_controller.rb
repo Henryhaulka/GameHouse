60e9defd252b2ef6e957147f37f4618e76e3fa48
Henry Onu <onuhenry24@gmail.com>
2021-07-28 14:36:58 +0100

    fix linters

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,8 +3,10 @@ class VotesController < ApplicationController
     if Vote.where(league_id: League.find(params[:league_id]), user_id: current_user.id).exists?
       return redirect_to root_path, notice: 'You already voted for this League '
     end
+
     @vote = League.find(params[:league_id]).votes.build(user_id: current_user.id)
-    return unless @vote.save redirect_to league_path(id: League.find(params[:league_id])),notice: 'Thanks!!! You have successfully voted for this League' 
+    return unless @vote.save redirect_to league_path(id: League.find(params[:league_id])),
+                                         notice: 'Thanks!!! You have successfully voted for this League'
   end
 
   def destroy
