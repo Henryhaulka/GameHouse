40c9a788c2d22e67355bb5899a3122c9634ac15f
Henry Onu <onuhenry24@gmail.com>
2021-07-28 12:18:50 +0100

    add cloudinary and configurations

--- app/models/league.rb
+++ app/models/league.rb
@@ -5,7 +5,10 @@ class League < ApplicationRecord
     belongs_to :author, class_name: 'User'
     validates :title, presence: true, length: { mininum: 3, maximum: 20}
     validates :text, presence: true, length: {maximum: 500}
-    has_attached_file :image, styles: { large: "1200x1000", medium: "300x300>"}, default_url: "/images/:style/missing.png"
+    has_attached_file :image, storage: :cloudinary,
+                              path: ':class/:id/:style/:filename'
+                              styles: { large: "1200x1000", medium: "300x300>"},
+                              default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
     def self.most_voted_league
