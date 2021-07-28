60e02e6aab31ebf74acd1f8153b4bf7a3198441a
Henry Onu <onuhenry24@gmail.com>
2021-07-28 13:54:23 +0100

    fix bug

--- app/models/league.rb
+++ app/models/league.rb
@@ -6,7 +6,7 @@ class League < ApplicationRecord
     validates :title, presence: true, length: { mininum: 3, maximum: 20}
     validates :text, presence: true, length: {maximum: 500}
     has_attached_file :image, storage: :cloudinary,
-                              path: ':class/:id/:style/:filename'
+                              path: ':class/:id/:style/:filename',
                               styles: { large: "1200x1000", medium: "300x300>"},
                               default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
