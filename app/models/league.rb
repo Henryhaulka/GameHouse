0d2edb250f45a3d4a39969bc3dd9e2ce70b77732
Henry Onu <onuhenry24@gmail.com>
2021-07-29 08:11:44 +0100

    fix linters

--- app/models/league.rb
+++ app/models/league.rb
@@ -5,8 +5,8 @@ class League < ApplicationRecord
   belongs_to :author, class_name: 'User'
   validates :title, presence: true, length: { mininum: 3, maximum: 20 }
   validates :text, presence: true, length: { maximum: 500 }
-  has_attached_file :image, :storage => :cloudinary,
-                            :path => ':id/:style/:filename',
+  has_attached_file :image, storage: :cloudinary,
+                            path: ':id/:style/:filename',
                             styles: { large: '1200x1000', medium: '300x300>' },
                             default_url: '/images/:style/missing.png'
   validates_attachment_content_type :image, content_type: %r{\Aimage/.*\z}
